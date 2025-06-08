a799270fd3aeb1bbacd0a112f416d6f2112e1beb
hg2411 <himanigoyal764@gmail.com>
2025-06-08 19:19:26 +0530

    edit page file

--- walletwatch/app/page.js
+++ walletwatch/app/page.js
@@ -2,8 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-  <div className="mt-40">
-     <Button variant="destructive">Made with ❤️ by AlgoNest</Button>;
-  </div>
+  <div></div>
   );
 }
