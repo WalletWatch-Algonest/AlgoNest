b8c72692b545e565e50769cf32289f0e6ef38fbc
mausumi25 <mausumirautaray2002@gmail.com>
2025-06-22 10:24:00 +0530

    transaction categories

--- walletwatch/app/(main)/account/[id]/page.jsx
+++ walletwatch/app/(main)/account/[id]/page.jsx
@@ -1,6 +1,8 @@
 import { getAccountWithTransactions } from "@/actions/accounts";
-import React from "react";
+import React, { Suspense } from "react";
 import { notFound } from "next/navigation";
+import TransactionTable from "../_components/transaction-table";
+import { BarLoader } from "react-spinners";
 
 const AccountsPage =async ({ params }) => {
   const accountData= await getAccountWithTransactions(params.id);
@@ -11,7 +13,8 @@ const AccountsPage =async ({ params }) => {
 
   const { transactions,...account }=accountData;
   return (
-    <div className="space-y-8 px-5 flex gap-4 items-end justify-between" >
+    <div className="space-y-8 px-5 " >
+      <div className="flex gap-4 items-end justify-between">
       <div>
         <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalize">
           {account.name}
@@ -25,8 +28,12 @@ const AccountsPage =async ({ params }) => {
         <div className="text-xsl  sm:text-2xl font-bold">${parseFloat(account.balance).toFixed(2)}</div>
         <p className="text-sm text-muted-foreground">{ account._count.transactions}Transactions </p>
       </div>
+      </div>
       {/* Chart Section*/}
       { /* Transaction Table*/}
+      <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}>
+        <TransactionTable transactions={transactions}/>
+      </Suspense>
     </div>
 
 
