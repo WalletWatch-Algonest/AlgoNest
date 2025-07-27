154ac16edf1db7c4fa83fb83b9ffbc39b8dd343c
mausumi25 <mausumirautaray2002@gmail.com>
2025-07-27 14:38:54 +0530

    edit tansaction complete

--- walletwatch/app/(main)/transaction/create/page.jsx
+++ walletwatch/app/(main)/transaction/create/page.jsx
@@ -2,30 +2,32 @@ import { getUserAccounts } from "@/actions/dashboard";
 import React from "react";
 import { defaultCategories } from "@/data/categories";
 import AddTransactionForm from "../_components/transaction-form";
-// import { getTransaction } from "@/actions/transactions";
+import { getTransaction } from "@/actions/transactions";
 
 // export default async function AddTransactionPage(props) {
-//   const { edit: editId } = await props.searchParams;
+ const { edit: editId } = await props.searchParams;
 
-const AddTransactionPage=async()=>{
+const AddTransactionPage=async(searchParams)=>{
 
 
   const accounts = await getUserAccounts();
+  const editId = searchParams?.edit;
 
-  // let initialData = null;
-  // if (editId) {
-  //   // const transaction = await getTransaction(editId);
-  //   // initialData = transaction;
-  // }
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
 
   return (
     <div className="max-w-3xl mx-auto px-5">
       {/* <h1 className="flex justify-center md:justify-normal mb-8"> */}
-        <h1 className="text-5xl gradient-title mb-8">Add Transaction</h1>
+        <h1 className="text-5xl gradient-title mb-8">{editId?"Edit":"Add"}</h1>
      
-    <AddTransactionForm accounts={accounts} categories={defaultCategories}/>
-    {/* </h1> */}
-      {/* You can render the form here using accounts and initialData if needed */}
+    <AddTransactionForm accounts={accounts} categories={defaultCategories}
+    editMode={!!editId}
+    initialData={initialData}
+    />
     </div>
   );
 };
