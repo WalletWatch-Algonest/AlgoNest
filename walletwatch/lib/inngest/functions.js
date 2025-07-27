63b97d8d48c679a09843de8111c298256d0e7b71
mausumi25 <mausumirautaray2002@gmail.com>
2025-07-27 13:59:11 +0530

    edit transaction

--- walletwatch/lib/inngest/functions.js
+++ walletwatch/lib/inngest/functions.js
@@ -251,3 +251,24 @@ function isTransactionDue(transaction) {
   // Compare with nextDue date
   return nextDue <= today;
 }
+
+function calculateNextRecurringDate(startDate, interval) {
+    const date = new Date(startDate);
+
+    switch (interval) {
+        case "DAILY":
+            date.setDate(date.getDate() + 1);
+            break;
+        case "WEEKLY":
+            date.setDate(date.getDate() + 7);
+            break;
+        case "MONTHLY":
+            date.setMonth(date.getMonth() + 1);
+            break;
+        case "YEARLY":
+            date.setFullYear(date.getFullYear() + 1);
+            break;
+    }
+
+    return date;
+}
